16dde2e3e97245adeafa6dcb0eebc68fb26747c5
Zaona <3409473369@qq.com>
2026-01-27 16:06:41 +0800

    feat: 添加底部导航栏和顶部AppBar

--- app/src/main/java/com/application/zaona/weather/MainActivity.kt
+++ app/src/main/java/com/application/zaona/weather/MainActivity.kt
@@ -4,16 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.application.zaona.weather.ui.theme.SimpleweathersyncerngTheme
+import top.yukonga.miuix.kmp.basic.MiuixScrollBehavior
+import top.yukonga.miuix.kmp.basic.NavigationBar
+import top.yukonga.miuix.kmp.basic.NavigationItem
 import top.yukonga.miuix.kmp.basic.Scaffold
-import top.yukonga.miuix.kmp.basic.SmallTopAppBar
+import top.yukonga.miuix.kmp.basic.TopAppBar
+import top.yukonga.miuix.kmp.basic.rememberTopAppBarState
+import top.yukonga.miuix.kmp.icon.MiuixIcons
+import top.yukonga.miuix.kmp.icon.extended.Send
+import top.yukonga.miuix.kmp.icon.extended.Settings
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,15 +35,41 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SimpleweathersyncerngTheme {
+                var selectedIndex by remember { mutableIntStateOf(0) }
+                val navItems = listOf(
+                    NavigationItem("同步", MiuixIcons.Send),
+                    NavigationItem("设置", MiuixIcons.Settings)
+                )
+                val topBarState = rememberTopAppBarState()
+                val scrollBehavior = MiuixScrollBehavior(state = topBarState)
+
                 Scaffold(
                     topBar = {
-                        SmallTopAppBar(title = "Simple Weather")
+                        TopAppBar(
+                            title = "简明天气同步器",
+                            scrollBehavior = scrollBehavior
+                        )
+                    },
+                    bottomBar = {
+                        NavigationBar(
+                            items = navItems,
+                            selected = selectedIndex,
+                            onClick = { selectedIndex = it }
+                        )
                     }
                 ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .nestedScroll(scrollBehavior.nestedScrollConnection),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        when (selectedIndex) {
+                            0 -> Greeting("同步")
+                            1 -> Greeting("设置")
+                        }
+                    }
                 }
             }
         }
